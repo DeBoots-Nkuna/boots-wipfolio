9534f077f156a1236b8ab699049275f1264ab294
Boots <61475589+DeBoots-Nkuna@users.noreply.github.com>
2025-06-02 07:07:45 +0200

    feat:implement navigation
    - included navigation between pages using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Project } from './pages/Project'
 import { Contact } from './pages/Contact'
-import { Routes, Route } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+import { RootLayout } from './layout/RootLayout'
 export const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Project />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    )
   )
+
+  return <RouterProvider router={router} />
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,24 @@
+import { NavLink } from 'react-router-dom'
 import btLogo from '../assets/boots-logo-bk.png'
 
 export const NavBar = () => {
   return (
     <header className="navbar-container">
       <img className="boots-logo" src={btLogo} alt="Nkuna Logo in Black" />
-      <nav>
-        <li>
-          <a>Home</a>
-        </li>
-        <li>
-          <a>Projects</a>
-        </li>
-        <li>
-          <a>Projects</a>
-        </li>
-        <li>
-          <a>Contact</a>
-        </li>
-      </nav>
-      <button>Get Started</button>
+      <ul>
+        <NavLink className="nav-link" to="/">
+          <li>Home</li>
+        </NavLink>
+        <NavLink className="nav-link" to="/about">
+          <li>About</li>
+        </NavLink>
+        <NavLink className="nav-link" to="projects">
+          <li>Projects</li>
+        </NavLink>
+        <NavLink className="nav-link" to="/contact">
+          <li>Contact</li>
+        </NavLink>
+      </ul>
     </header>
   )
 }
